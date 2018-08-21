11784953cd586d5d967692a37a644bc227251a7c
王乐天 <koh.718@gmail.com>
2018-08-21 12:17:58 +0800

    new plugins

--- mybatis-ext-core/src/main/java/com/github/superkoh/mybatis/annotation/QueryExprType.java
+++ mybatis-ext-core/src/main/java/com/github/superkoh/mybatis/annotation/QueryExprType.java
@@ -3,10 +3,10 @@ package com.github.superkoh.mybatis.annotation;
 public enum QueryExprType {
 
   EQUAL("="),
-  GREATER_THAN(">"),
-  GREATER_AND_EQUAL_THAN(">="),
-  LESS_THAN("<"),
-  LESS_AND_EQUAL_THAN("<=");
+  GREATER_THAN("&gt;"),
+  GREATER_AND_EQUAL_THAN("&gt;="),
+  LESS_THAN("&lt;"),
+  LESS_AND_EQUAL_THAN("&lt;=");
 
   private String expr;
 
